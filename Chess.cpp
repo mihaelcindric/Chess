113c948dfa76e02cd2d0d510b113ae62ed2f1900
Mihael Cindric <Mihael Cindric@DESKTOP-E5KB2IR>
2023-08-17 22:42:05 +0200

    Add project files.

---
+++ Chess.cpp
@@ -0,0 +1,12 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <SDL.h>
+
+int main()
+{
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        printf("Error initializing SDL: %s\n", SDL_GetError());
+        return 1;
+    };
+}
