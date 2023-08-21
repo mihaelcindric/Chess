9c5257f3d1f449a2b80f627988692ee74f352941
Mihael Cindrić <mihael.cindri9@gmail.com>
2023-08-21 20:22:32 +0200

    Added selected figure change and movement rule function for pawn

--- Chess.cpp
+++ Chess.cpp
@@ -6,9 +6,17 @@
 #include "SDL.h"
 #include "SDL_image.h"
 
+#define MAX_MOVES 8
+
+typedef struct {
+    int row;
+    int col;
+} Position;
+
 void drawBoard(SDL_Renderer* renderer, char game[8][8]);
 void updateBoard(SDL_Renderer* renderer, char game[8][8], int oldX, int oldY, int newX, int newY);
 int handlePieceMovement(SDL_Renderer* renderer, char game[8][8], int* isMoving, int* startRow, int* startCol);
+Position* getPawnMoves(char game[8][8], int startRow, int startCol, int* count);
 
 
 int main(int argc, char* argv[])
@@ -201,13 +209,66 @@ int handlePieceMovement(SDL_Renderer* renderer, char game[8][8], int* isMoving, 
                 }
             }
             else {
-                char piece = game[*startRow][*startCol];
-                game[*startRow][*startCol] = ' ';
-                game[row][col] = piece;
-                *isMoving = 0; // Resetiranje statusa pomicanja
-                updateBoard(renderer, game, *startCol, *startRow, col, row); // Poziv updateBoard umjesto drawBoard
+                // Provjerite jesu li figure iste boje
+                char selectedPiece = game[*startRow][*startCol];
+                char targetPiece = game[row][col];
+                if ((isupper(selectedPiece) && isupper(targetPiece)) ||
+                    (islower(selectedPiece) && islower(targetPiece))) {
+                    // Mijenjanje odabrane figure
+                    *startRow = row;
+                    *startCol = col;
+                }
+                else {
+                    char piece = game[*startRow][*startCol];
+                    game[*startRow][*startCol] = ' ';
+                    game[row][col] = piece;
+                    *isMoving = 0; // Resetiranje statusa pomicanja
+                    updateBoard(renderer, game, *startCol, *startRow, col, row); // Poziv updateBoard
+                }
             }
         }
     }
     return 1; // Znači da korisnik ne želi zatvoriti prozor
+}
+
+
+Position* getPawnMoves(char game[8][8], int startRow, int startCol, int* count) {
+    Position* moves = (Position*)malloc(MAX_MOVES * sizeof(Position));
+    *count = 0;
+
+    char pawn = game[startRow][startCol];
+    int direction = islower(pawn) ? 1 : -1; // Za crne pješake ide dolje, za bijele ide gore
+
+    // Provjerite može li se pomaknuti naprijed
+    if (game[startRow + direction][startCol] == ' ') {
+        moves[*count].row = startRow + direction;
+        moves[*count].col = startCol;
+        (*count)++;
+    }
+
+    // Provjerite može li se pojesti neka figura dijagonalno
+    if (startCol - 1 >= 0 && ((isupper(pawn) && islower(game[startRow + direction][startCol - 1])) ||
+        (islower(pawn) && isupper(game[startRow + direction][startCol - 1])))) {
+        moves[*count].row = startRow + direction;
+        moves[*count].col = startCol - 1;
+        (*count)++;
+    }
+
+    if (startCol + 1 < 8 && ((isupper(pawn) && islower(game[startRow + direction][startCol + 1])) ||
+        (islower(pawn) && isupper(game[startRow + direction][startCol + 1])))) {
+        moves[*count].row = startRow + direction;
+        moves[*count].col = startCol + 1;
+        (*count)++;
+    }
+
+    // Posebna pravila za početni pomak od dva polja za pješake
+    if ((isupper(pawn) && startRow == 6) || (islower(pawn) && startRow == 1)) {
+        if (game[startRow + direction][startCol] == ' ' && game[startRow + 2 * direction][startCol] == ' ') {
+            moves[*count].row = startRow + 2 * direction;
+            moves[*count].col = startCol;
+            (*count)++;
+        }
+    }
+
+    return moves;
 }
